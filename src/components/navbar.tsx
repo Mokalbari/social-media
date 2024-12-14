ac68a211c8baba9dfe10d0543780e2c03fc9834a
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-12-14 08:42:36 +0100

    add navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export function Navbar() {
+  return (
+    <nav className="bg-blue-500 flex items-center justify-between text-white font-bold p-4">
+      <Link href={"/"}>
+        <div className="max-w-14">
+          <Image
+            src="/react.svg"
+            alt="React space logo"
+            width={800}
+            height={800}
+          />
+        </div>
+      </Link>
+      <ul className="flex items-center space-x-4">
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/users"}>Users</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
