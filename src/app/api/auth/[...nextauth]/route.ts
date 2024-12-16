39731bb6c43c202a99e8cfc86ca0b2c231565d19
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-12-16 17:14:53 +0100

    add prisma model for user and session + connection to neon db

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,10 @@
+import { prisma } from "@/lib/prisma"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -16,3 +19,8 @@ export { handler as GET, handler as POST }
 /* La route + slug permet de catch toutes les routes get psot signin signout par nextauth
 Le fichier de configuration est simple : rajouter les providers
 dans le .env se situent toute les clés pour les API nécessaires*/
+
+/* L'adaptateur prisma permet de faire le pont entre Next Auth et la base de données neon + prisma
+Le prisma adaptateur vient avec un model de base à remplir dans ./prisma/schema.prisma
+Ce model peut être récupérer sur : https://next-auth.js.org/v3/adapters/prisma
+*/
